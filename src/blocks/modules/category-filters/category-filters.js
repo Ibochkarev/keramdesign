7f81a57c0be3ad6dc85062565d48e62b8a21debc
Bochkarev Ivan <ivanx86@gmail.com>
2022-01-10 01:23:08 +0600

    Добавлена страница категории

---
+++ src/blocks/modules/category-filters/category-filters.js
@@ -0,0 +1,13 @@
+modules.define('category-filters', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
